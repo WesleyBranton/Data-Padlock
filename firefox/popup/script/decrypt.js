121bf0a68e903a0c08d4d8f3672e3d67f91ed135
Wesley Branton <addons@wesleybranton.com>
2019-05-31 18:01:56 -0400

    Added license headers

--- firefox/popup/script/decrypt.js
+++ firefox/popup/script/decrypt.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 document.getElementById('return').addEventListener('click',menu);
 document.getElementById('load').addEventListener('click',decryptMsg);
 document.getElementById('secret').addEventListener('keyup',verify);
