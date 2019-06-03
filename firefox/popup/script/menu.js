2411e18a1d8b95b6099d24041a5e019af95769f8
Wesley Branton <addons@wesleybranton.com>
2019-06-03 09:48:08 -0400

    Added menu button press handler

---
+++ firefox/popup/script/menu.js
@@ -0,0 +1,15 @@
+var button = document.getElementsByTagName('button');
+for (i = 0; i < button.length; i++) {
+	button[i].addEventListener('click',openPage);
+}
+
+// Opens page
+function openPage(e) {
+	var page = e.target.id;
+	if (page == 'read') {
+		document.getElementById('loader').className = 'purple';
+	}
+	document.getElementById('menu-buttons').className = 'hide';
+	document.getElementById('loading-screen').className = '';
+	window.location = page + ".html";
+}
