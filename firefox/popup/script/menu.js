8478518add1ff0022e60a7f96e229305b23e558d
Wesley Branton <addons@wesleybranton.com>
2019-06-03 16:19:21 -0400

    Added terms of service link

--- firefox/popup/script/menu.js
+++ firefox/popup/script/menu.js
@@ -1,3 +1,4 @@
 document.getElementById('send').addEventListener('click',function(){nav('send')});
 document.getElementById('read').addEventListener('click',function(){nav('read')});
 document.getElementById('about').addEventListener('click',function(){nav('about')});
+document.getElementById('terms').addEventListener('click',function(){window.open('https://addons.mozilla.org/firefox/addon/secure-send/eula/','_blank')});
